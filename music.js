426bdd4a0bc7299c3eeb9098a09bfed92cb6700e
Yukina Ogasawara <s1f101900723@iniad.org>
2022-12-16 18:31:18 +0900

    a

--- music.js
+++ music.js
@@ -8,6 +8,7 @@ function getParam(name, url) {
     return name+"="+decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 var globalapidata;
+var apistatus="off";
 function load_api(){
     var apiurl='https://script.google.com/macros/s/AKfycbzCQSJ-7N4oCs6sAi63fJmbl9it2DEZbbP0drv20H3FtulEg6yGbFcsEuMY0WOD96t4XQ/exec?getdata=all';
     fetch(apiurl) // APIのURL
@@ -43,6 +44,7 @@ function load_api2(paramguidename){
         window.myLineChart.data.datasets[0].data = listresult;
         window.myLineChart.data.datasets[2].data = listresult;
         globalapidata=listresult;
+        apistatus="on";
         load_melo();
 
 
@@ -89,19 +91,22 @@ function load_api4(paramguidename,paramname){
         window.myLineChart.data.datasets[0].data = listresult;
         window.myLineChart.data.datasets[2].data = listresult;
         globalapidata=listresult;
+        apistatus="on";
         load_melo();
 
     });
 }
 
 function load_melo(){
-    if(melomode==1){
-        window.myLineChart.data.datasets[1].data = globalapidata.map(i => [(i)*melodis/5, (i)*melodis/5+melodis-0.1]);
-    }else{
-        window.myLineChart.data.datasets[1].data = globalapidata.map(i => [parseInt(Math.log(i/27.5)/Math.log(2**(0.083)))*melodis, melodis-0.1+parseInt(Math.log(i/27.5)/Math.log(2**(0.083)))*melodis]);
+    if(apistatus=="on"){
+        if(melomode==1){
+            window.myLineChart.data.datasets[1].data = globalapidata.map(i => [(i)*melodis/5, (i)*melodis/5+melodis-0.1]);
+        }else{
+            window.myLineChart.data.datasets[1].data = globalapidata.map(i => [parseInt(Math.log(i/27.5)/Math.log(2**(0.083)))*melodis, melodis-0.1+parseInt(Math.log(i/27.5)/Math.log(2**(0.083)))*melodis]);
+        }
+        window.myLineChart.update();
+        createchart1(document.getElementById('outputArea').value.split(/\r\n|\n/).map(Number));
     }
-    window.myLineChart.update();
-    createchart1(document.getElementById('outputArea').value.split(/\r\n|\n/).map(Number));
 }
 //console.log(melodis);
 //console.log(melolist.map(i => parseInt(Math.log(i/27.5)/Math.log(2**(0.083)))*melodis));
